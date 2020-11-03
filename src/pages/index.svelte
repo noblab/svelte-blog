0a22ba933dc7c78b5b7c6929c09090b1b648a797
nano <nakaharanoboru@gmail.com>
2020-11-03 11:03:37 +0900

    initial commit

---
+++ src/pages/index.svelte
@@ -0,0 +1,21 @@
+<div class="hero">
+  <h1>あさあけまち</h1>
+  <code>
+    <p>日の出を待ってる頭の中で</p>
+    <p>あぶくのようなはかない考えを</p>
+    <p>ちょっとだけ残しておく</p>
+  </code>
+
+  <figure>
+    <img alt="霧、つとめて" src="images/toppage/fog.jpg" />
+    <figcaption>
+      Illustration thanks to
+      <a
+        href="https://pixabay.com/ja/users/cafepampas-610842/"
+        target="_blank">cafepampas</a>
+    </figcaption>
+  </figure>
+</div>
+
+<!-- routify:options index=10 -->
+<!-- routify:options title="home" -->
