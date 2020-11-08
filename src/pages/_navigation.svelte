59a1cecadd9402dc0dc62495903909ca19942489
nano <nakaharanoboru@gmail.com>
2020-11-08 23:40:15 +0900

    onegai

--- src/pages/_navigation.svelte
+++ src/pages/_navigation.svelte
@@ -2,8 +2,10 @@
   import { layout, url, isActive } from "@sveltech/routify";
 </script>
 
-<nav class="header">
-  {#each $layout.parent.children as { path, title }}
-    <a href={$url(path)} class:active={$isActive(path)}>{title}</a>
-  {/each}
-</nav>
+<div>
+  <nav class="header">
+    {#each $layout.parent.children as { path, title }}
+      <a href={$url(path)} class:active={$isActive(path)}>{title}</a>
+    {/each}
+  </nav>
+</div>
